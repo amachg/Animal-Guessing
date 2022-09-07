88083dc207813799dd80e7d16ad5092060bf8f72
amachg <amach@sch.gr>
2022-09-07 14:57:09 +0300

    Add project files.

---
+++ Skepsou-Zoo.cpp
@@ -0,0 +1,25 @@
+﻿// Skepsou-Zoo.cpp : Defines the entry point for the application.
+// Conversion from Univac-BASIC code AUTH/Math, 16/12/86
+
+#include <vector>
+#include "Skepsou-Zoo.h"
+
+using namespace std;
+
+int main()
+{
+	vector<string> a;
+	string b;
+	do {
+		cout << "Σκεψου κάποιο Ζώο.... Δώσε Υ όταν είσαι έτοιμος" << endl;
+		cin >> b;
+	} while (b != "Y");
+
+	int i = 1;
+	int a1;
+	auto& ra = a[i];
+	if (i > 1)
+		a1 = 1;
+
+	return 0;
+}
