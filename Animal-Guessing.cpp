5a43b848d542942f416777161b3348cdba2d140f
Α Μ <amach@sch.gr>
2022-09-11 01:04:12 +0300

    wireup

--- Animal-Guessing.cpp
+++ Animal-Guessing.cpp
@@ -6,11 +6,10 @@
 #include <string>
 #include <vector>
 #include <algorithm>	//all_of
-//#include <regex>
 
 struct Element {
 	std::string question_or_animal;
-	struct {
+	struct Indices {
 		size_t yes, no;
 	} indices;
 };
@@ -23,9 +22,9 @@ std::vector<Element> database {
 
 int main() {
 	std::cout << "Please think of an animal.... Press Enter key when ready.\n";
-	//std::cout << "Σκέψου κάποιο Ζώο.... Πάτα το πλήκτρο Enter όταν είσαι έτοιμος\n";
 	std::cin.ignore();
 
+	// Ask all the stored questions
 	size_t index{ 0 };
 	char typed_key{};
 	do {
@@ -33,8 +32,7 @@ int main() {
 		const auto& question = element.question_or_animal;
 		std::cout << question << '\n';
 		do {
-			std::cout << "Please answer Yes (y) or No (n)\n"; 
-			//Πάτα Y αν συμφωνείς ή Ν αν διαφωνείς.
+			std::cout << "Please answer Yes (y) or No (n)\n";
 			std::cin >> typed_key;
 		} while (!std::cin.fail() && typed_key != 'y' && typed_key != 'n');
 
@@ -47,6 +45,7 @@ int main() {
 		}
 	} while (index < database.size());
 
+	// Has the animal been found?
 	const auto& element = database[index];
 	const std::string animal = element.question_or_animal;
 	std::cout << "Is it a " << animal << "?\n";
@@ -60,37 +59,36 @@ int main() {
 		return EXIT_SUCCESS;
 	}
 
+	// Add new animal to database
 	std::string new_animal;
-	//const std::regex words("\\w+");
 	do {
 		new_animal.clear();
 		std::cout << "Please type the name of the animal you have in your mind..\n";
 		std::getline(std::cin >> std::ws, new_animal);	// can take spaces between words
-		//std::cout << "getline returns: \t" << std::quoted(new_animal) << '\n';
-	} while ( !std::cin.fail() && !std::ranges::all_of(new_animal,
-		[](unsigned char c) { return std::isalpha(c)  || std::isblank(c); })
-		//!std::regex_match(new_animal, words)
-		);
-	const Element new_element{ new_animal , {0,0} };
-	database.emplace_back(new_element);
+	} while (!std::cin.fail() && !std::ranges::all_of(new_animal,
+		[](unsigned char c) { return std::isalpha(c) || std::isblank(c); }));
+	const Element new_DB_animal{ new_animal , {0,0} };
+	const auto& ref = database.emplace_back(new_DB_animal);;
+	const auto new_animal_index = &ref - database.data();
+	std::cout << new_animal_index;
 
+	// Add new question to database
 	std::string new_question;
 	do {
-		new_question.clear();		
+		new_question.clear();
 		std::cout << "Now, type in a question which differentiates "
 			<< std::quoted(new_animal) << " from " << std::quoted(animal) << ".\n";
 		std::getline(std::cin >> std::ws, new_question);
 	} while (!std::cin.fail() && new_question.empty());
 
+	// Wire up new and last questions
 	do {
 		std::cout << "For the " << std::quoted(new_animal) << ", does the question "
 			<< std::quoted(new_question) << " get answered with a Yes(y), or No(n)?\n";
 		std::cin >> typed_key;
 	} while (!std::cin.fail() && typed_key != 'y' && typed_key != 'n');
-
-	const auto new_question_index = (typed_key == 'y') ?
-
-		element.indices.yes :
-		element.indices.no;
-
+	const Element::Indices new_question_indices = (typed_key == 'y') ?
+		{ new_animal_index, 0 } : { 0, new_animal_index };
+	const Element new_DB_question{ new_question , new_question_indices };
+	database.emplace_back(new_DB_question);
 }
